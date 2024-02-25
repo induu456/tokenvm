4d732398d41727b4864cc692243fff4a3d139c62
induu456 <142425292+induu456@users.noreply.github.com>
2024-02-25 14:54:16 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "indchain"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "iduu"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "IND"
 )
 
 var ID ids.ID
